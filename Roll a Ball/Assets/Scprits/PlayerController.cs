e614fcdb661962d25c2bc5fd1710c4f9f2da2f7e
Epithach <antoine.thach@gmail.com>
2016-09-20 19:03:18 +0200

    Update : Adding score + winning message + every file

---
+++ Roll a Ball/Assets/Scprits/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody       rb;
+    public float            moveHorizontal;
+    public float            moveVertical;
+    public float            speed;
+    public Text             countText;
+    public Text             winText;
+    private int             count;
+    public Text             TIME;
+    public float            timer;
+    public string           timerFormatted;
+    System.TimeSpan         t;
+
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        winText.text = "";
+        TIME.text = "";
+    }
+
+    void FixedUpdate() {
+        this.moveHorizontal = Input.GetAxis("Horizontal");
+        this.moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(this.moveHorizontal, 0.0f, this.moveVertical);
+
+        rb.AddForce(movement * this.speed);
+
+
+    //    t = System.TimeSpan.FromSeconds(timer);
+
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+        }
+    }
+
+    void setCountText() {
+        countText.text = "Count : " + count.ToString();
+        if (count == 12)
+        {
+            winText.text = "YOU WIN MY BRO";
+//            timerFormatted = string.Format("{0:D2}h:{1:D2}m:{2:D2}s:{3:D3}ms", t.Hours, t.Minutes, t.Seconds, t.Milliseconds);
+  //          TIME.text = timerFormatted;
+        }
+
+    }
+}
+ 
